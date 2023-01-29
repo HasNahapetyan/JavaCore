3f3881f5fef1d42a759b8fea5844f332c27d4ef9
Hasmik <1hasmiknahapetyan@gmail.com>
2023-01-29 15:12:09 +0400

    stack extend

--- src/ITSpaceAcademy/homeworks/braceCheck/CharStack.java
+++ src/ITSpaceAcademy/homeworks/braceCheck/CharStack.java
@@ -2,6 +2,7 @@ package ITSpaceAcademy.homeworks.braceCheck;
 
 public class CharStack {
     private char arr[] = new char [10];
+    private int size = 10;
     private int tos;
 
     CharStack(){
@@ -10,13 +11,19 @@ public class CharStack {
 
     void push(char item){
         if(tos == 9){
-            System.out.println("hmi kgrem");
+            extend();
         }
         else {
             arr[++tos] = item;
         }
     }
-
+    private void extend() {
+        char[] array1 = new char[size+10];
+        for (int i = 0; i < size; i++) {
+            array1[i] = arr[i];
+        }
+        arr = array1;
+    }
     char pop(){
         if(tos<0){
             System.err.println("CharStack underflow,");
