6a7e3a0031b64bd6ef1e7ae861fecaca0f1aa6d9
Hasmik <1hasmiknahapetyan@gmail.com>
2023-02-12 21:17:10 +0400

    employee2

--- src/ITSpaceAcademy/homeworks/employee/EmployeeDemo.java
+++ src/ITSpaceAcademy/homeworks/employee/EmployeeDemo.java
@@ -4,25 +4,10 @@ import java.util.Scanner;
 
 public class EmployeeDemo {
 
-    private static EmployeeStorage storage = new EmployeeStorage();
-    private static Scanner sc = new Scanner(System.in);
+    private static final EmployeeStorage storage = new EmployeeStorage();
+    private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        /*
-        0 for exit (0 սեղմելու դեպքում ծրագիրը վերջանալու է)
-        1 add employee (1 սեղմելու դեպքում ուզելու է բոլոր տվյալները աշխատակից սարքելու, ու սարքելու է իրա օբյեկտը ու քցի EmployeeStorage-ի մեջ)
-        2 print all employee(տպելու է բոլոր աշխատակիցներին)
-        3 search employee by employee ID (փնտրելու է աշխատակից իր իդ-ով)
-        4 search employee by company name  (տալու ենք ընկերության անունը, տպի իրա բոլոր աշխատակիցներին)*/
-
-        /*
-        Please input 5 for search employee by salary range (ընտրելուց հետո երկու հատ double թիվ կուզեք կոնսոլով, ու իրանց միջակայքում գտնվող salary ունեցող employee-ներին կտպեք )
-        Please input 6 for change employee position by id - իդ-ով աշխատակցի position-ը կփոխեք։
-        Please input 7 for print only active employees (տպում եք այն աշխատակիցներին, ովքեր active=true)
-        Please input 8 for inactive employee by id - ներմուծում ենք employeeID ու եթե կա էդ այդի ով աշխատակից, իրա active-ը դարձնում ենք false.
-        Please input 9 for activate employee by id - ներմուծում ենք employeeID ու եթե կա էդ այդի ով աշխատակից ու իրա active=false, իրա active-ը դարձնում ենք true.
-        */
-
         boolean isRun = true;
 
         while(isRun){
@@ -40,47 +25,34 @@ public class EmployeeDemo {
 
             String command = sc.nextLine();
 
-            switch (command){
-                case "0":
-                    isRun = false;
-                    break;
-                case "1":
-                    addEmployee();
-                    break;
-                case "2":
-                    storage.print();
-                    break;
-                case "3":
-                    getById();
-                    break;
-                case "4":
+            switch (command) {
+                case "0" -> isRun = false;
+                case "1" -> addEmployee();
+                case "2" -> storage.print();
+                case "3" -> getById();
+                case "4" -> {
                     System.out.println("Please input company");
                     String company = sc.nextLine();
                     storage.searchEmployeesByCompany(company);
-                    break;
-                case "5":
-                    searchEmployeeBySalaryRange();
-                    break;
-                case "6":
+                }
+                case "5" -> searchEmployeeBySalaryRange();
+                case "6" -> {
                     System.out.println("Please input id");
                     String id = sc.next();
                     System.out.println(storage.changeEmployeePositionById(id));
-                    break;
-                case "7":
-                    storage.printOnlyActiveEmployees();
-                    break;
-                case "8":
+                }
+                case "7" -> storage.printOnlyActiveEmployees();
+                case "8" -> {
                     System.out.println("Please input id");
                     String emplId = sc.next();
                     storage.inactiveEmployeeById(emplId);
-                    break;
-                case "9":
+                }
+                case "9" -> {
                     System.out.println("Please input id");
                     String empId = sc.next();
                     storage.activateEmployeeById(empId);
-                    break;
-                default:
-                    System.out.println("wrong command, please try again");
+                }
+                default -> System.out.println("wrong command, please try again");
             }
         }
 
